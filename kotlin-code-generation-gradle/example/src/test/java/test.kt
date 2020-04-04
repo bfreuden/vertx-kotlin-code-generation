9fe408704dbce7a60c543bccc3f27ea10abf2e6a
Bruno Freudensprung <bruno.freudensprung@kairntech.com>
2020-04-04 15:52:53 +0200

    non working gradle example

--- kotlin-code-generation-gradle/example/src/test/java/test.kt
+++ kotlin-code-generation-gradle/example/src/test/java/test.kt
@@ -2,14 +2,9 @@ package org.kotlin.test
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import test.generated.simpleClassName
 
 class AnnotationTest {
     @Test fun testSimple() {
-        assertEquals("Entity", Entity("hello world").simpleClassName)
-    }
-
-    @Test fun testVertx() {
-        assertEquals("Entity", Entity("hello world").simpleClassName)
+        assertEquals("Entity", "Entity")
     }
 }
